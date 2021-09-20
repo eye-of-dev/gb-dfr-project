990ea109f76210ce8ca8f46b42fa2499b0e5a9e2
Anton Lisikov <anton.lisikov@gmail.com>
2021-09-20 20:25:49 +0300

    feet: Урок 2. Введение в React. Компонентный подход

--- library/library/settings.py
+++ library/library/settings.py
@@ -37,13 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'corsheaders',
+
     'authors',
     'users'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,3 +132,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.Users'
+
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:3000",
+]
